ebbdba92ec63b7f500554e804116ac90a79cbe00
Paula Sophia Santoyo Arteaga <A01745312@itesm.mx>
2022-10-25 11:55:20 -0500

    prueba

--- servidor.py
+++ servidor.py
@@ -41,12 +41,8 @@ def modeloForm ():
     # Utilizar el modelo
     resultado = dt.predict(datosEntrada.reshape(1, -1))
 
-<<<<<<< HEAD
-    return jsonify({"Resultado": "datos recibidos"})
-=======
 
     return jsonify({"Resultado": str(resultado[0])})
->>>>>>> parent of 55b4f17 (pruebaRes)
 
 # *****  modeloFile  *****
 
