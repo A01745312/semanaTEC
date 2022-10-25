3a014caf33585363b15e52e5e8b5ab23339a0e3f
Paula Sophia Santoyo Arteaga <A01745312@itesm.mx>
2022-10-25 09:59:52 -0500

    actualizacion

--- servidor.py
+++ servidor.py
@@ -1,4 +1,11 @@
+from crypt import methods
+from fileinput import filename
+
 from flask import Flask, request, jsonify, render_template
+from werkzeug.utils import secure_filename
+from joblib import load
+import numpy as np
+import os
 
 
 # Generar servidor (Back-end)
@@ -6,12 +13,32 @@ from flask import Flask, request, jsonify, render_template
 servidorWeb = Flask(__name__)
 
 # Anotacion
-
 @servidorWeb.route("/test", methods = ['GET'])
-
-
 def formulario ():
     return render_template('pagina.html')
+
+# Procesar datos a traves del form
+@servidorWeb.route("/modeloIA", methods = ['POST'])
+def modeloForm ():
+    # Procesar datos de entrada
+    contenido = request.form
+    print(contenido)
+
+    return jsonify({"Resultado": "datos recibidos"})
+
+
+# Procesar datos de un archivo
+@servidorWeb.route("/modeloFile", methods = ['POST'])
+def modeloFile ():
+    # Procesar datos de entrada
+    f = request.files['file']
+    filename = secure_filename(f.filename)
+    path = os.path.join(os.getcwd, 'files', filename)
+    f.save(path)
+    file = open(path,'r')
+    for line in file:
+        print(line)
+    return jsonify({"Resultado": "datos recibidos"})
 
 
 if __name__ == '__main__':
